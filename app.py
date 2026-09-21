99b029b6949df264f0150a938b313eb3fcb01895
unifiedconsultancy <info@unifiedtechnology.ca>
2026-09-21 11:02:51 -0600

    changes

--- app.py
+++ app.py
@@ -1,21 +1,72 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-app=FastAPI()
-#GET,POST,PUT,PATCH
-class User(BaseModel):
-    name:str
-    age:int
-    email:str
-@app.post("/users")
-def create_user(user:User):
-    return {
-        "message":"user created",
-        "user":user
-    }
-@app.get("/")
-def home():
-    return {"message":"HEllo from api"}
-@app.get("/contact")
-def contact():
-    return {"message":"Welcome to contact"}
-#about,Test
+import streamlit as st
+
+st.title("My Streamlit App")
+
+st.write("Welcome to my Streamlit app!")
+
+name=st.text_input("Enter your name:")
+st.write(f"Hello, {name}!")
+
+st.button("Click me!")
+
+st.chat_input("Hi enter the prompt here")
+
+st.sidebar.title("Sidebar")
+
+##Basic Calculator using streamlit
+st.sidebar.header("Basic Calculator")   
+
+# Page configuration
+st.set_page_config(
+    page_title="Python Calculator",
+    page_icon="🧮",
+    layout="centered"
+)
+
+# Application title
+st.title("🧮 Simple Calculator")
+
+st.write("A calculator built using Python and Streamlit.")
+
+# User input
+num1 = st.number_input(
+    "Enter First Number",
+    value=0.0
+)
+
+num2 = st.number_input(
+    "Enter Second Number",
+    value=0.0
+)
+
+# Select operation
+operation = st.selectbox(
+    "Select Operation",
+    ["Addition (+)",
+     "Subtraction (-)",
+     "Multiplication (*)",
+     "Division (/)"]
+)
+
+# Calculate button
+if st.button("Calculate", type="primary"):
+
+    if operation == "Addition (+)":
+        result = num1 + num2
+
+    elif operation == "Subtraction (-)":
+        result = num1 - num2
+
+    elif operation == "Multiplication (*)":
+        result = num1 * num2
+
+    elif operation == "Division (/)":
+
+        if num2 == 0:
+            st.error("Cannot divide by zero!")
+            st.stop()
+
+        result = num1 / num2
+
+    # Display result
+    st.success(f"Result: {result:,.2f}")
--- pydantic_p.py
+++ pydantic_p.py
@@ -1,13 +1,15 @@
 from pydantic import BaseModel
 
+#API request validation using Pydantic
+
 class Student(BaseModel):
     name: str
     age: int
     course: str
 
 student= Student(
-    name="John Doe", 
-    age=20, 
+    name="Amir Ali", 
+    age=56, 
     course="Computer Science")
 
 print(student.name)  # Output: John Doe
